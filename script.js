6c1dc52d354dd47053bd48e77a28f104d5c5370a
aman-student2001 <akumaraman1101@gmail.com>
2026-03-06 01:36:05 +0530

    add new localRepo

---
+++ script.js
@@ -0,0 +1,7 @@
+const nested= [[1,2,3]
+             [4,5,6]
+             [7,8,9]];
+
+             for(let i=0;i<nested[i].lenght; j++){
+                console.log(nested[i][j]);
+             }
